787824b954c98dff5f26586c615c809e0fbab455
Manuela <m.munaretto@gmail.com>
2018-07-18 12:43:20 +0200

    add sign up stub

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,4 +12,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get contact_path
     assert_select "title", full_title("Contact")
   end
+
+  test "signup link" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", signup_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
 end
